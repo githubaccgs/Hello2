aa03480d25cb184a8f12b7141ad0cc07b940dca9
githubaccgs <42225515+githubaccgs@users.noreply.github.com>
2019-01-30 17:40:20 -0600

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "this script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+
+echo "this was run automagically"
+echo "This is the new line ......"
+echo " adding new comment2"
+echo "adding new comment3"
+echo "adding new comment4"
+echo "adding new comment5"
